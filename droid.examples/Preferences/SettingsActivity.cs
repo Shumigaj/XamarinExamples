59d6af15350a9354e59f716e648bd54d12a5f889
Pavel Shumihai <shumigaj@gmail.com>
2019-02-07 20:33:01 +0300

    apps(preferences): Initial commit

---
+++ droid.examples/Preferences/SettingsActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Preferences;
+using Android.Support.V7.Preferences;
+using System.Collections.Generic;
+
+namespace droid.examples.Preferences
+{
+    [Activity(Label = "Settings activity", Theme = "@style/AppTheme", Name = "droid.examples.Preferences.SettingsActivity")]
+    [IntentFilter(new string[] { "android.intent.action.APPLICATION_PREFERENCES" })]
+    public class SettingsActivity : PreferenceActivity
+    {
+        public override void OnBuildHeaders(IList<Header> target)
+        {
+            base.OnBuildHeaders(target);
+            LoadHeadersFromResource(Resource.Xml.preference_headers, target);
+        }
+
+        public class SettingsFragment : PreferenceFragmentCompat
+        {
+            public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
+            {
+                // Load the Preferences from the XML file
+                //AddPreferencesFromResource(Resource.Xml.app_preferences);
+                SetPreferencesFromResource(Resource.Xml.app_preferences, rootKey);
+            }
+        }
+    }
+
+}
